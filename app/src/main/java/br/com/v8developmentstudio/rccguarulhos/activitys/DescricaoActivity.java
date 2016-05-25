8d3fec12e840e74430f8eb98161446d4175870d3
cleitondantas <cleitonpanao@gmail.com>
2016-05-25 14:31:25 -0300

    Adicionado Compartilhar e add no Calendario

--- app/src/main/java/br/com/v8developmentstudio/rccguarulhos/activitys/DescricaoActivity.java
+++ app/src/main/java/br/com/v8developmentstudio/rccguarulhos/activitys/DescricaoActivity.java
@@ -1,19 +1,13 @@
 package br.com.v8developmentstudio.rccguarulhos.activitys;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.animation.ValueAnimator;
-import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.Point;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -22,14 +16,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.TimeZone;
 
 import br.com.v8developmentstudio.rccguarulhos.R;
 import br.com.v8developmentstudio.rccguarulhos.adapter.ScaleImageView;
@@ -43,8 +34,7 @@ import br.com.v8developmentstudio.rccguarulhos.services.CalendarEventService;
 import br.com.v8developmentstudio.rccguarulhos.task.DownloadImagesTask;
 import br.com.v8developmentstudio.rccguarulhos.util.Constantes;
 
-import android.provider.CalendarContract.Calendars;
-import android.provider.CalendarContract.Events;
+import android.widget.Toast;
 
 /**
  * Created by cleiton.dantas on 18/03/2016.
@@ -62,8 +52,9 @@ public class DescricaoActivity extends AppCompatActivity {
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
     private AppBarLayout appBarLayout;
     private CalendarEventService calendarEventService;
-
-
+    private FloatingActionButton fabMenu, fabShare,fabAddCalendar;
+    private TextView textViewSumario,textViewDescricao,textViewDataHoraInicio,textViewDataHoraFim,textViewLocal;
+    private ScaleImageView thumbnail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,14 +73,54 @@ public class DescricaoActivity extends AppCompatActivity {
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         mCollapsingToolbarLayout.setExpandedTitleColor(Color.TRANSPARENT); // transperent color = #00000000
-        mCollapsingToolbarLayout.setTitle("Informações");
 
-        TextView textViewSumario = (TextView) findViewById(R.id.idsumario);
-        TextView textViewDescricao = (TextView) findViewById(R.id.idDescricao);
-        TextView textViewDataHoraInicio = (TextView) findViewById(R.id.idDataHoraInicio);
-        TextView textViewDataHoraFim = (TextView) findViewById(R.id.idDataHoraFim);
-        TextView textViewLocal = (TextView) findViewById(R.id.idLocal);
-        ScaleImageView thumbnail = (ScaleImageView) findViewById(R.id.thumbnail);
+        textViewSumario = (TextView) findViewById(R.id.idsumario);
+        textViewDescricao = (TextView) findViewById(R.id.idDescricao);
+        textViewDataHoraInicio = (TextView) findViewById(R.id.idDataHoraInicio);
+        textViewDataHoraFim = (TextView) findViewById(R.id.idDataHoraFim);
+        textViewLocal = (TextView) findViewById(R.id.idLocal);
+        thumbnail = (ScaleImageView) findViewById(R.id.thumbnail);
+
+        final Animation animeFloating = AnimationUtils.loadAnimation(this, R.animator.rotate);
+        final Animation animeFloating2 = AnimationUtils.loadAnimation(this, R.animator.rotate2);
+        fabMenu = (FloatingActionButton) findViewById(R.id.idFabMenu);
+        fabShare = (FloatingActionButton) findViewById(R.id.idFabShare);
+        fabAddCalendar = (FloatingActionButton) findViewById(R.id.idFabAddCalendar);
+        fabShare.hide();
+        fabAddCalendar.hide();
+        fabMenu.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if(View.GONE == fabShare.getVisibility()) {
+                   fabMenu.startAnimation(animeFloating);
+                   fabShare.show();
+                   fabAddCalendar.show();
+               }else{
+                   fabMenu.startAnimation(animeFloating2);
+                   fabShare.hide();
+                   fabAddCalendar.hide();
+               }
+           }
+       });
+
+        fabShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String shareBody = prepereShare(evento);
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,evento.getSumario());
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent,"RCC Share"));
+            }
+        });
+
+        fabAddCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEventoLocalCalendar();
+            }
+        });
 
         evento = getEventoDao(id);
         calendario = persistenceDao.recuperaConfigCalendarPorID(evento.getIdCalendario());
@@ -110,13 +141,15 @@ public class DescricaoActivity extends AppCompatActivity {
             thumbnail.setMinimumWidth(width);
             appBarLayout.setExpanded(true);
         }else{
-            int width = size.x;
-            int height = size.y/2;
+            int width = size.x/4;
+            int height = size.y/4;
             thumbnail.setMaxHeight(height);
             thumbnail.setMinimumHeight(height);
             thumbnail.setMaxWidth(width);
             thumbnail.setMinimumWidth(width);
             appBarLayout.setExpanded(false);
+            appBarLayout.setActivated(false);
+
         }
 
         textViewSumario.setText(evento.getSumario());
@@ -130,9 +163,31 @@ public class DescricaoActivity extends AppCompatActivity {
 
     private void addEventoLocalCalendar(){
         calendarEventService.addEventoAoCalendarioLocal(evento);
+        Toast.makeText(this,"EVENTO ADD NO CALENDARIO LOCAL", Toast.LENGTH_SHORT).show();
 
     }
 
+    private String prepereShare(Evento evento){
+       final SimpleDateFormat dateFormat2 = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        String msg ="";
+
+        if(evento.getSumario()!=null){
+            msg+=evento.getSumario()+"\n \n";
+        }
+        if(evento.getDescricao()!=null){
+            msg+=evento.getDescricao()+"\n";
+        }
+        if(evento.getLocal()!=null){
+            msg+="LOCAL: "+evento.getLocal()+"\n";
+        }
+        if(evento.getDataHoraInicio()!=null){
+            msg+="INICIO: "+dateFormat2.format(evento.getDataHoraInicio())+"\n";
+        }
+        if(evento.getDataHoraFim()!=null){
+            msg+="FIM: "+dateFormat2.format(evento.getDataHoraFim());
+        }
+        return msg;
+    }
 
     private Evento getEventoDao(int id) {
         return persistenceDao.recuperaEventoPorID(id);
@@ -165,9 +220,6 @@ public class DescricaoActivity extends AppCompatActivity {
                 return true;
             case android.R.id.home:
                 onBackPressed();
-                return true;
-            case R.id.addCalendar:
-                addEventoLocalCalendar();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
