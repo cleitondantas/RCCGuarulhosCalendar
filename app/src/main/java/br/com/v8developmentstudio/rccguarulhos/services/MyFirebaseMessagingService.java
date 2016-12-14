cd005a3eb20716c46d7c22c3a7177b8f1090794b
cleitondantas <cleitonpanao@gmail.com>
2016-12-14 09:57:10 -0200

    Ajustado configurações para o Firebase Service Messenger

--- app/src/main/java/br/com/v8developmentstudio/rccguarulhos/services/MyFirebaseMessagingService.java
+++ app/src/main/java/br/com/v8developmentstudio/rccguarulhos/services/MyFirebaseMessagingService.java
@@ -43,10 +43,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-
+        sendNotification(remoteMessage.getFrom());
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
         }
 
         // Check if message contains a notification payload.
